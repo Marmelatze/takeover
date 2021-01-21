df4a0a7c7665cf96c92a45942adf8be83aa20097
Florian Pfitzer <florian@pfitzer.me>
2021-01-21 10:00:35 +0100

    redirect

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header('Location: https://daimler-tss.com');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8" />
+		<title>Index</title>
+	</head>
+	<body>
+    Domain taken over by Daimler TSS		
+	</body>
+</html>
